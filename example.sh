6dd06a50f8832010c3fd10574bb93db1eb0a1431
Chinmay G Khasnis <chinmaykhasanis@gmail.com>
2020-05-29 20:45:35 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -6,6 +6,8 @@ python -m train --bmname=ENZYMES --assign-ratio=0.1 --hidden-dim=30 --output-dim
 
 # ENZYMES - Diffpool -trl 1
 python -m train --bmname=ENZYMES --assign-ratio=0.1 --hidden-dim=20 --output-dim=20 --cuda=1 --num-classes=2 --method=soft-assign --batch_size=18
+# ENZYMES - Diffpool -trl 2
+python -m train --bmname=ENZYMES --assign-ratio=0.1 --hidden-dim=20 --output-dim=20 --cuda=1 --num-classes=2 --method=soft-assign --batch_size=20
 
 # DD
 python -m train --datadir=data --bmname=DD --cuda=0 --max-nodes=500 --epochs=1000 --num-classes=2
